6550c15a91c74d7a63f6c6237740914377b31230
Fritz <fre.healy@gmail.com>
2017-12-04 15:18:28 -0500

    changing home pull

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php $post = get_post(37);
+			<?php $post = get_post(6);
 			/* Start the Loop */
 			if ( $post ) : setup_postdata();
 				get_template_part( 'template-parts/content', "index" );
